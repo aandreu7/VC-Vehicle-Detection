70f2b3191254333d03d030b7f5938f9d67b5ff51
Andreu <aplana16ins@gmail.com>
2025-02-27 09:55:14 +0100

    Mitjana i desviació típica
    Codi que calcula la mitjana i la desviació típica de les imatges del conjunt d'entrenament.

--- prac1.m
+++ prac1.m
@@ -1,3 +1,8 @@
+clearvars,
+close all,
+clc,
+
+
 % ====================== CARREGAR IMATGES ======================
 
 folder = './highway/input';
@@ -25,3 +30,38 @@ disp('Carga completa de imágenes.');
 
 im_train = images(1:150);
 im_test = images(151:300);
+
+
+% ====================== CÀLCUL MITJANA I DESVIACIÓ TÍPICA ======================
+
+[M, N] = size(im_train{1});
+
+num_images = size(im_train, 2);
+
+mean_image = zeros(M, N, 'double');
+sd_image = zeros(M, N, 'double');    
+
+% Mitjana
+for i = 1:num_images
+    mean_image = mean_image + double(im_train{i});
+end
+mean_image = mean_image / num_images;
+
+% Desviació estàndard
+for i = 1:num_images
+    sd_image = sd_image + (double(im_train{i}) - mean_image).^2;
+end
+sd_image = sqrt(sd_image / num_images);
+
+mean_image = uint8(mean_image);
+sd_image = uint8(sd_image);
+
+figure(1);
+imshow(mean_image);
+title('Imatge de la mitjana');
+
+figure(2);
+imshow(sd_image);
+title('Imatge de la desviació estàndard');
+
+
