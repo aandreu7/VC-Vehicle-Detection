dff1c877db657623522243e5eb394b03c30c7740
Andreu <aplana16ins@gmail.com>
2025-03-13 10:03:38 +0100

    Codi modulat

--- prac1.m
+++ prac1.m
@@ -39,100 +39,104 @@ disp('Carga completa de imágenes.');
 im_train = images(1:150);
 im_test = images(151:300);
 
-
 % ====================== CÀLCUL MITJANA I DESVIACIÓ TÍPICA ======================
 
-% Mitjana
-images_stack = cat(3, im_train{:});
-mean_image = mean(double(images_stack), 3);
-
-% Desviació estàndard
-sd_image = std(double(images_stack), 0, 3);
-
-mean_image = uint8(mean_image);
-sd_image = uint8(sd_image);
-
-figure(1);
-imshow(mean_image);
-title('Imatge de la mitjana');
-
-figure(2);
-imshow(sd_image);
-title('Imatge de la desviació estàndard');
-
-%% ====================== SEGMENTACIÓ BÀSICA ======================
-
-thr = 40;
-
-segmentation_images = cell(1, size(im_train, 2));
-for x=1:size(im_train, 2)
-    segmentation_images{x} = abs(im_train{x} - mean_image) > thr;
+function [mean_image, sd_image] = compute_mean_and_sd(im)
+    % Mitjana
+    images_stack = cat(3, im{:});
+    mean_image = mean(double(images_stack), 3);
+    
+    % Desviació estàndard
+    sd_image = std(double(images_stack), 0, 3);
+    
+    mean_image = uint8(mean_image);
+    sd_image = uint8(sd_image);
+    
+    figure(1);
+    imshow(mean_image);
+    title('Imatge de la mitjana');
+    
+    figure(2);
+    imshow(sd_image);
+    title('Imatge de la desviació estàndard');
 end
 
-% Imatge de prova
-figure(3);
-imshow(segmentation_images{7});
-title('Segmentació bàsica')
+%% ====================== SEGMENTACIÓ BÀSICA ======================
+function basic_segmentation = segment_basic(im, mean_image)
+    thr = 40;
+    
+    segmentation_images = cell(1, size(im, 2));
+    for x=1:size(im, 2)
+        segmentation_images{x} = abs(im{x} - mean_image) > thr;
+    end
+    
+    % Imatge de prova
+    figure(3);
+    imshow(segmentation_images{7});
+    title('Segmentació bàsica')
+end
 
 
 %% ====================== SEGMENTACIÓ AVANÇADA ======================
 
-% Primera aproximació (1 si |Ii,j − µi,j | > ασi,j + β, 0 en cas contrari)
+function segmentation_images = segment_images(im, mean_image, sd_image)
+    a = 0.15 * sd_image;
+    b = 5;
 
-a = 0.15 * sd_image;
-b = 5;
+    filter_image = sd_image;
+    filter_image(filter_image < 35) = 130;
 
-filter_image = sd_image;
-filter_image(filter_image < 35) = 130; % Blanquejem la imatge per millorar els resultats (opcional)
+    threshold = a + b;
+    adjusted_mean = mean_image - filter_image;
 
-threshold = a + b;
-adjusted_mean = mean_image - filter_image;
+    segmentation_images = cell(1, size(im, 2));
+    for x = 1:size(im, 2)
+        diff_image = abs(im{x} - adjusted_mean) > threshold;
+        segmentation_images{x} = adjusted_mean - uint8(diff_image * 255);
+        segmentation_images{x}(segmentation_images{x} > b) = 255;
+    end
 
-segmentation_images = cell(1, size(im_train, 2));
-for x = 1:size(im_train, 2)
-    diff_image = abs(im_train{x} - adjusted_mean) > threshold;
-    segmentation_images{x} = adjusted_mean - uint8(diff_image * 255);
-    segmentation_images{x}(segmentation_images{x} > b) = 255;
+    figure(4);
+    subplot(1, 2, 1);
+    imshow(segmentation_images{7});
+    subplot(1, 2, 2);
+    imshow(im{7});
+    sgtitle('Segmentació avançada: primera aproximació');
 end
-
-
-% Imatge de prova
-figure(4);
-subplot(1, 2, 1);
-imshow(segmentation_images{7});
-subplot(1, 2, 2);
-imshow(im_train{7});
-sgtitle('Segmentació avançada: primera aproximació');
 
 %% ================================ OPENING ===========================
  
- SE = strel("disk",1);
- 
- SE2 = strel('diamond',1);
- 
- opened_images = cell(1, size(im_train, 2));
- for x = 1:size(im_train, 2)
-     opened_images{x} = imdilate(imerode(segmentation_images{x},SE),SE2);
- end
- figure(5)
- subplot(1, 2, 1);
- imshow(segmentation_images{7});
- subplot(1, 2, 2);
- imshow(opened_images{7});
- sgtitle('Opening: Before and after');
+function opened_images = apply_opening(im, segmentation_images)
+    SE = strel("disk", 1);
+    SE2 = strel('diamond', 1);
+
+    opened_images = cell(1, size(im, 2));
+    for x = 1:size(im, 2)
+        opened_images{x} = imdilate(imerode(segmentation_images{x}, SE), SE2);
+    end
+
+    figure(5)
+    subplot(1, 2, 1);
+    imshow(segmentation_images{7});
+    subplot(1, 2, 2);
+    imshow(opened_images{7});
+    sgtitle('Opening: Before and after');
+end
+
+%% ===================== VIDEO AMB TEST IMAGES ============================
+
+[mean_image, sd_image] = compute_mean_and_sd(im_test);
+segmentation_images_test = segment_images(im_test, mean_image, sd_image);
+opened_images_test = apply_opening(im_test, segmentation_images_test);
+
 
 
 %% ========================= AVALUACIÓ ====================================
 
-accuracy = zeros(1, length(opened_images));
-precision = zeros(1, length(opened_images));
-recall = zeros(1, length(opened_images));
-f1_score = zeros(1, length(opened_images));
-sensitivity = zeros(1, length(opened_images));
-specificity = zeros(1, length(opened_images));
+accuracy = zeros(1, length(opened_images_test));
 
-for i = 1:length(opened_images)
-    segmented = opened_images{i};
+for i = 1:length(opened_images_test)
+    segmented = opened_images_test{i};
     gt = images_gt{i};
 
     gt = uint8(gt > 0) * 255;
