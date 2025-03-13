d5a28417104812eae5c5c8c43b6e8375c549b082
Andreu <aplana16ins@gmail.com>
2025-03-13 09:50:09 +0100

    Avaluació Accuracy

--- prac1.m
+++ prac1.m
@@ -7,8 +7,12 @@ clc,
 
 folder = './highway/input';
 files = dir(fullfile(folder, 'in*.jpg'));
+folder_gt = './highway/groundtruth';
+files_gt = dir(fullfile(folder_gt, 'gt*.png'));
 
 images = cell(1, 1350 - 1051 + 1);
+images_gt = cell(1, 1350 - 1051 + 1);
+
 index = 1;
 
 for i = 1:length(files)
@@ -19,6 +23,10 @@ for i = 1:length(files)
         filename = fullfile(files(i).folder, name);
         im_color = imread(filename);
         images{index} = rgb2gray(im_color);
+
+        filename_gt = fullfile(folder_gt, sprintf('gt%06d.png', num));
+        images_gt{index} = imread(filename_gt);
+
         index = index + 1;
     end
 end
@@ -33,8 +41,9 @@ im_test = images(151:300);
 
 
 % ====================== CÀLCUL MITJANA I DESVIACIÓ TÍPICA ======================
-images_stack = cat(3, im_train{:});
 
+% Mitjana
+images_stack = cat(3, im_train{:});
 mean_image = mean(double(images_stack), 3);
 
 % Desviació estàndard
@@ -56,8 +65,8 @@ title('Imatge de la desviació estàndard');
 thr = 40;
 
 segmentation_images = cell(1, size(im_train, 2));
-for x=1:size(images, 2)
-    segmentation_images{x} = abs(images{x} - mean_image) > thr;
+for x=1:size(im_train, 2)
+    segmentation_images{x} = abs(im_train{x} - mean_image) > thr;
 end
 
 % Imatge de prova
@@ -79,10 +88,10 @@ filter_image(filter_image < 35) = 130; % Blanquejem la imatge per millorar els r
 threshold = a + b;
 adjusted_mean = mean_image - filter_image;
 
-segmentation_images = cell(1, size(images, 2));
-for x = 1:size(images, 2)
-    diff_image = abs(images{x} - adjusted_mean) > threshold;
-    segmentation_images{x} = adjusted_mean - uint8(diff_image * 255); %im2uint8(diff_image); %
+segmentation_images = cell(1, size(im_train, 2));
+for x = 1:size(im_train, 2)
+    diff_image = abs(im_train{x} - adjusted_mean) > threshold;
+    segmentation_images{x} = adjusted_mean - uint8(diff_image * 255);
     segmentation_images{x}(segmentation_images{x} > b) = 255;
 end
 
@@ -92,32 +101,55 @@ figure(4);
 subplot(1, 2, 1);
 imshow(segmentation_images{7});
 subplot(1, 2, 2);
-imshow(images{7});
+imshow(im_train{7});
 sgtitle('Segmentació avançada: primera aproximació');
 
 %% ================================ OPENING ===========================
+ 
+ SE = strel("disk",1);
+ 
+ SE2 = strel('diamond',1);
+ 
+ opened_images = cell(1, size(im_train, 2));
+ for x = 1:size(im_train, 2)
+     opened_images{x} = imdilate(imerode(segmentation_images{x},SE),SE2);
+ end
+ figure(5)
+ subplot(1, 2, 1);
+ imshow(segmentation_images{7});
+ subplot(1, 2, 2);
+ imshow(opened_images{7});
+ sgtitle('Opening: Before and after');
 
-SE = strel('rectangle', [3, 3]);
 
-SE2 = strel('rectangle', [3, 3]); 
+%% ========================= AVALUACIÓ ====================================
 
-
-opened_images = cell(1, size(images, 2));
-for x = 1:size(images, 2)
-    opened_images{x} = imdilate(imerode(segmentation_images{x},SE),SE2);
-end
-figure(5)
-subplot(1, 2, 1);
-imshow(segmentation_images{7});
-subplot(1, 2, 2);
-imshow(opened_images{7});
-
-%% ============================== GRAVAR VIDEO ==========================
-video = VideoWriter('video_output.avi', 'Motion JPEG AVI');
-video.FrameRate = 20;  
-open(video);
+accuracy = zeros(1, length(opened_images));
+precision = zeros(1, length(opened_images));
+recall = zeros(1, length(opened_images));
+f1_score = zeros(1, length(opened_images));
+sensitivity = zeros(1, length(opened_images));
+specificity = zeros(1, length(opened_images));
 
 for i = 1:length(opened_images)
-    writeVideo(video, opened_images{i});
+    segmented = opened_images{i};
+    gt = images_gt{i};
+
+    gt = uint8(gt > 0) * 255;
+
+    TP = sum(segmented(:) == 1 & gt(:) == 255);
+    TN = sum(segmented(:) == 0 & gt(:) == 0);
+    FP = sum(segmented(:) == 1 & gt(:) == 0);
+    FN = sum(segmented(:) == 0 & gt(:) == 255);
+
+    total_pixels = numel(segmented);
+    correct_predictions = TP + TN;
+    
+    accuracy(i) = correct_predictions / total_pixels;
 end
-close(video);
+
+mean_accuracy = mean(accuracy);
+
+fprintf('Accuracy mitjà: %.4f\n', mean_accuracy);
+
+
